8d77977918b049d2e3821e87816aa3bab0306bb3
Rui Conti <rconti@microsoft.com>
2021-10-19 15:06:38 +0000

    Accommodate for easier pathogen loading

--- vim/clone_refs.sh
+++ vim/clone_refs.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-for ref in $(cat ./refs/sources.txt); do git clone $ref plugins/$(echo $ref | awk -F'[/@.]' '{ print $4 }'); done
+refs=./refs/sources.txt
+output=bundles
+for ref in $(cat $refs); do dirname=$(echo $ref | awk -F'[/@.]' '{ print $4 }') && test ! -d $output/$dirname && git clone $ref $output/$dirname; done
