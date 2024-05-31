eb6ab466c83701ef9ec9c39727fe692f132df1c8
Rui <rui@conti.sh>
2024-05-30 22:05:19 -0300

    add .config

--- vim/clone_refs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-refs=./refs/sources.txt
-output=bundles
-for ref in $(cat $refs); do dirname=$(echo $ref | awk -F'[/@.]' '{ print $4 }') && test ! -d $output/$dirname && git clone $ref $output/$dirname; done
--- vim/post_install.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-cd plugins/coc && yarn
